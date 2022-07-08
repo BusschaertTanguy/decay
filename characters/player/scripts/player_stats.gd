555d8d356a52fc01c92a26b1193f6321c669087e
Thibmachien <93033687+Thibmachien@users.noreply.github.com>
2022-07-08 19:28:57 +0200

    As a Player, I want to move in 8 directions #2

--- characters/player/scripts/player_stats.gd
+++ characters/player/scripts/player_stats.gd
@@ -1,7 +1,5 @@
 class_name PlayerStats
 extends Resource
 
-export(float) var health: float;
-export(float) var damage: float;
 export(float) var speed: float;
-export(float) var dick_size: float;
+
